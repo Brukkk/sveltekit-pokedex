000db82e9d976b7c59d97ded5a0c6ee3c8843344
Bruk Thinkpad <bruktorra@gmail.com>
2023-02-15 11:57:25 -0300

    first component as a nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
+<script>
+    import Nav from "../components/nav.svelte";
+</script>
 <svelte:head>
     <title>
         Svelte Pokedex
     </title>
 </svelte:head>
 <h1>Svelte Kit Pokedex</h1>
+<Nav/>
 <p>Visit <a href="https://www.youtube.com/watch?v=UU7MgYIbtAk&ab_channel=JamesQQuick">James Q Quick</a> to learn more!</p>
-<p>Click <a href="/about">here</a> to know more!</p>
+
  <style>
     p {
         color: brown;
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,10 @@
+<script>
+    import Nav from "../../components/nav.svelte";
+</script>
 <svelte:head>
     <title>
         About the Svelte Pokedex
     </title>
 </svelte:head>
 <h1>About section</h1>
+<Nav/>
