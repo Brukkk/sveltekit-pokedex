e16cae7d084987ad66e82bfcf63e50ee2dca5c87
Bruk Thinkpad <bruktorra@gmail.com>
2023-02-23 12:14:09 -0300

    Svelte store

--- src/stores/pokestore.js
+++ src/stores/pokestore.js
@@ -1,3 +1,7 @@
+import {writable} from "svelte/store";
+
+export const pokemonList = writable([]);
+
 const fetchPokemon = async () => {
     const url = 'https://pokeapi.co/api/v2/pokemon?limit=150';
     const res = await fetch(url);
@@ -9,4 +13,9 @@ const fetchPokemon = async () => {
             image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
         }
     })
+    // Once we have our writable store to export, we set it with the information from the fetch function
+    pokemonList.set(loadedPokemon);
+    // Now we can consume this pokemonList in other components
 }
+// To make sure everything happens, we need to call our fetch function
+fetchPokemon();
