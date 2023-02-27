b30405e39149a195fc5d0f6cf752e6ec463c1747
Bruk Thinkpad <bruktorra@gmail.com>
2023-02-27 12:32:35 -0300

    Bind input, filtered list and reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,18 @@
     import PokemonCard from "../components/pokemonCard.svelte";
     // $ sign is to actual access the data inside the writable store
     // console.log($pokemonList)
+    let searchTerm = "";
+    let filteredPokemon = [];
+
+    $ : {
+        // This will re-run as part of the reactivity any time something changes in this scope
+        console.log(searchTerm)
+        if (searchTerm){
+            filteredPokemon = $pokemonList.filter(poke => poke.name.includes(searchTerm.toLowerCase()))
+        } else {
+            filteredPokemon = [...$pokemonList]
+        }
+    }
 </script>
 <svelte:head>
     <title>
@@ -10,9 +22,9 @@
     </title>
 </svelte:head>
 <h1 class="text-4xl font-bold underline text-center my-8 uppercase">Svelte Kit Pokedex</h1>
-<input type="text" placeholder="Search your pokemon" class="w-full bg-slate-50 rounded-lg p-4 text-lg border border-slate-300 mb-2 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300">
+<input bind:value={searchTerm} type="text" placeholder="Search your pokemon" class="w-full my-8 bg-slate-50 rounded-lg p-4 text-lg border border-slate-300 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300" >
 <div class="grid grid-cols-2 gap-8 sm:grid-cols-3">
-    {#each $pokemonList as pokemon }
+    {#each filteredPokemon as pokemon }
         <PokemonCard pokemonInfo={pokemon} />
     {/each}
 </div>
