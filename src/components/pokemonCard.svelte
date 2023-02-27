07bd61beb4f7ae9f52d3340902b46e20b725b7da
Bruk Thinkpad <bruktorra@gmail.com>
2023-02-27 12:35:05 -0300

    dynamic pages for each pokemon ID

--- src/components/pokemonCard.svelte
+++ src/components/pokemonCard.svelte
@@ -2,9 +2,9 @@
     // this expects information as the props of the component
     export let pokemonInfo;
 </script>
-<div class="bg-slate-200 opacity-75 hover:opacity-100 transition-opacity ease-in-out hover:-translate-y-1 hover:scale-110 p-2">
+<a href={`/pokemon/${pokemonInfo.id}`} class="bg-slate-200 opacity-75 hover:opacity-100 transition-opacity ease-in-out hover:-translate-y-1 hover:scale-110 p-2">
     <p class="text-center text-2xl uppercase"> <span>{pokemonInfo.id}. </span>
         {pokemonInfo.name}</p>
     <img class="mx-auto aspect-square h-40 w-40" src={pokemonInfo.image} alt={`${pokemonInfo.name} sprite`}>
 
-</div>
+    </a>
---
+++ src/routes/pokemon/[slug]/+page.svelte
@@ -0,0 +1,1 @@
+<h1>This is a page with url and parameter according to the pokemon's id</h1>
