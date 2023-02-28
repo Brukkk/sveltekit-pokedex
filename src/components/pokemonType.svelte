18470ec0ed9fdc8b61333201409d5daf11b62f27
Bruk Thinkpad <bruktorra@gmail.com>
2023-02-28 12:00:32 -0300

    Information displayed + tailwind playground + type pokemon component

---
+++ src/components/pokemonType.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let type;
+
+    $: borderTypeColor = type.type.name === "normal" ? "border-neutral-300" :
+        type.type.name === "fighting" ? "border-amber-800" :
+        type.type.name === "flying" ? "border-violet-300" :
+        type.type.name === "poison" ? "border-fuchsia-700" :
+        type.type.name === "ground" ? "border-amber-400" :
+        type.type.name === "rock" ? "border-amber-200":
+        type.type.name === "bug" ? "border-lime-300":
+        type.type.name === "ghost" ? "border-purple-700" :
+        type.type.name === "steel" ? "border-grey-300":
+        type.type.name === "fire" ? "border-red-500":
+        type.type.name === "water" ? "border-blue-500":
+        type.type.name === "grass" ? "border-lime-600":
+        type.type.name === "electric" ? "border-yellow-300":
+        type.type.name === "psychic" ? "border-rose-400":
+        type.type.name === "ice" ? "border-cyan-300":
+        type.type.name === "dragon" ? "border-violet-600":
+        type.type.name === "dark" ? "border-grey-700":
+        type.type.name === "fairy" ? "border-pink-400":
+        type.type.name === "shadow" ? "border-purple-900" :
+        "";
+
+</script>
+
+<span class="inline-block px-2 border-4 rounded-xl {borderTypeColor}">{type.type.name}</span>
--- src/routes/pokemon/[slug]/+page.svelte
+++ src/routes/pokemon/[slug]/+page.svelte
@@ -2,7 +2,7 @@
     // universal data prop
     // https://kit.svelte.dev/docs/load#page-data
     /** @type {import('./$types').PageData} */
-
+    import PokemonType from '../../../components/pokemonType.svelte';
     export let data;
     console.log(data)
 </script>
@@ -11,4 +11,14 @@
         Pokedex: {data.pokeData.name.charAt(0).toUpperCase() + data.pokeData.name.slice(1)}
     </title>
 </svelte:head>
-<h1>{data.pokeData.name.toUpperCase()}</h1>
+<h1 class="text-3xl text-center">{data.pokeData.name.toUpperCase()} <span class="text-slate-500"> N° {data.pokeData.id}</span></h1>
+<div class="flex flex-col items-center">
+    <img class="h-40 w-40" src={data.pokeData.sprites.other["official-artwork"]["front_default"]} alt={`Pokemon ${data.pokeData.name} official default artwork`}>
+    <div class="flex gap-2">
+    {#each data.pokeData.types as type }
+            <PokemonType {type} />
+    {/each}
+    </div>
+    <p>Height: <span>{data.pokeData.height / 10} m</span></p>
+    <p>Weight: <span>{(data.pokeData.weight / 10).toFixed(1)} kg</span></p>
+</div>
