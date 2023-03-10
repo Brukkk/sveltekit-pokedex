6f01ff0bde555b9684a4f59c64ae1ee9d65fde6e
Bruk Thinkpad <bruktorra@gmail.com>
2023-03-10 17:43:21 -0300

    card grid style update

--- src/components/pokemonCard.svelte
+++ src/components/pokemonCard.svelte
@@ -2,9 +2,12 @@
     // this expects information as the props of the component
     export let pokemonInfo;
 </script>
-<a href={`/pokemon/${pokemonInfo.id}`} class="bg-slate-200 opacity-75 hover:opacity-100 transition-opacity ease-in-out hover:-translate-y-1 hover:scale-110 p-2">
-    <p class="text-center text-2xl uppercase"> <span>{pokemonInfo.id}. </span>
-        {pokemonInfo.name}</p>
-    <img class="mx-auto aspect-square h-40 w-40" src={pokemonInfo.image} alt={`${pokemonInfo.name} sprite`}>
+<a href={`/pokemon/${pokemonInfo.id}`} class="bg-slate-200 rounded-lg opacity-75 hover:opacity-100 transition-opacity ease-in-out hover:-translate-y-1 hover:scale-110 p-2">
+    <div class="relative">
+        <span class="absolute top-2 right-2 font-semibold">#{pokemonInfo.id}</span>
+        <img class="mx-auto aspect-square h-40 w-40" src={pokemonInfo.image} alt={`${pokemonInfo.name} sprite`}>
+        <p class="text-center text-2xl first-letter:uppercase">{pokemonInfo.name}</p>
 
-    </a>
+    </div>
+    
+</a>
