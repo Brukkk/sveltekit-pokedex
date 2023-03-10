6e3719c973ab48546930bcfd0e943aa50c52fbf8
Bruk Thinkpad <bruktorra@gmail.com>
2023-03-10 16:20:35 -0300

    style title for pokemon route

--- src/routes/pokemon/[slug]/+page.svelte
+++ src/routes/pokemon/[slug]/+page.svelte
@@ -4,15 +4,16 @@
     /** @type {import('./$types').PageData} */
     import PokemonType from '../../../components/pokemonType.svelte';
     export let data;
-    console.log(data)
+    // console.log(data)
 </script>
 <svelte:head>
     <title>
         Pokedex: {data.pokeData.name.charAt(0).toUpperCase() + data.pokeData.name.slice(1)}
     </title>
 </svelte:head>
-<h1 class="text-3xl text-center">{data.pokeData.name.toUpperCase()} <span class="text-slate-500"> N° {data.pokeData.id}</span></h1>
+<h1 class="text-3xl text-center">{data.pokeData.name.toUpperCase()}</h1>
 <div class="flex flex-col items-center">
+    <span class="block text-slate-500">{data.pokeData.id}</span>
     <img class="h-40 w-40" src={data.pokeData.sprites.other["official-artwork"]["front_default"]} alt={`Pokemon ${data.pokeData.name} official default artwork`}>
     <div class="flex gap-2">
     {#each data.pokeData.types as type }
