f86e5c8324a5f4c04e10d904dcb7b97230f0bb22
Bruk Thinkpad <bruktorra@gmail.com>
2023-03-15 17:47:53 -0300

    missed key on each loop to iterate the filtered pokemons
    As elements were deleted in the doom, bg colors were messed up with the ones who were still included in the array

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,6 @@
 
     $ : {
         // This will re-run as part of the reactivity any time something changes in this scope
-        console.log(searchTerm)
         if (searchTerm){
             filteredPokemon = $pokemonList.filter(poke => poke.name.includes(searchTerm.toLowerCase()))
         } else {
@@ -24,7 +23,7 @@
 <h1 class="text-4xl font-bold underline text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 <input bind:value={searchTerm} type="text" placeholder="Search your pokemon" class="w-full my-8 bg-slate-50 rounded-lg p-4 text-lg border border-slate-300 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300" >
 <div class="grid grid-cols-2 gap-8 sm:grid-cols-3">
-    {#each filteredPokemon as pokemon }
+    {#each filteredPokemon as pokemon (pokemon.id) }
         <PokemonCard pokemonInfo={pokemon} />
     {/each}
 </div>
