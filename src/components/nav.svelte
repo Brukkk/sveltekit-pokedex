3631b0a1ae6401a38aea6abed0c8c9d1b906f8a7
Bruk Thinkpad <bruktorra@gmail.com>
2023-03-17 03:16:17 -0300

    svg fixed

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,10 +1,9 @@
-<script>
-    import SearchIcon from "../assets/static/searchIcon.svg";
-</script>
 <nav class=" text-white flex w-full justify-around items-center gap-2 fixed bottom-0 left-0 right-0 pb-2 bg-cyan-600 z-10 shadow-lg">
     <a  class="text-xl" href="/">Home</a>
     <a href="#search" class="flex items-center justify-center rounded-full p-2 z-10 -mt-4 bg-cyan-600">
-        <img src={SearchIcon} alt="" class="h-8 w-8 stroke-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-white">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
     </a>
     <a class="text-xl" href="/about">About</a>
 </nav>
