a17fa33b1b7a6444a46e68d07eef82d912165340
Bruk Thinkpad <bruktorra@gmail.com>
2023-04-14 11:37:08 -0300

    onMount loaded store for conditional rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script>
+    import { onMount } from "svelte";
     import {pokemonList} from "../stores/pokestore"
     import PokemonCard from "../components/pokemonCard.svelte";
+    import { fade } from 'svelte/transition';
     // $ sign is to actual access the data inside the writable store
     // console.log($pokemonList)
     let searchTerm = "";
-    let filteredPokemon = [];
+    let filteredPokemon = null;
+    let storeLoaded;
+
+    onMount(() => {
+        pokemonList.subscribe((value) => {
+            storeLoaded =  value;
+        });
+    });
 
     $ : {
         // This will re-run as part of the reactivity any time something changes in this scope
@@ -25,7 +34,11 @@
     <input bind:value={searchTerm} type="text" placeholder="Search your pokemon" class="w-full my-8 bg-slate-50 rounded-lg p-4 text-lg border border-slate-300 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300" >
 </section>
 <div class="grid grid-cols-1 gap-8 sm sm:grid-cols-2 lg:grid-cols-3">
-    {#each filteredPokemon as pokemon (pokemon.id) }
-        <PokemonCard pokemonInfo={pokemon} />
-    {/each}
+    {#if !storeLoaded}
+        <p transition:fade="{{ duration: 2000}}">Loading...</p>
+    {:else}
+        {#each filteredPokemon as pokemon (pokemon.id) }
+            <PokemonCard pokemonInfo={pokemon} />
+        {/each}
+    {/if}
 </div>
