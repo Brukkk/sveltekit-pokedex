9990bbdf22d5c9bd6edb2687a6c877fb060e44f3
Bruk Thinkpad <bruktorra@gmail.com>
2023-06-22 18:15:30 -0300

    better performance and lazy loading

--- src/components/pokemonCard.svelte
+++ src/components/pokemonCard.svelte
@@ -15,7 +15,7 @@
 <a href={`/pokemon/${pokemonInfo.id}`} class="group/item block w-full h-full {bgColor} {fromColor} {toColor} rounded-lg lg:opacity-75 lg:hover:opacity-100 transition-opacity lg:hover:-translate-y-1 lg:hover:scale-110">
     <div class="relative">
         <span class="absolute text-slate-600 top-1 right-2 font-semibold text-2xl">#{pokemonInfo.id}</span>
-        <img class="mx-auto aspect-square h-40 w-40" src={pokemonInfo.image} alt={`${pokemonInfo.name} sprite`}>
+        <img loading="lazy" class="mx-auto aspect-square h-40 w-40" src={pokemonInfo.image} alt={`${pokemonInfo.name} sprite`}>
         <div class="py-2 rounded-b-lg bg-gradient-to-t from-slate-500 ">
             <p class=" text-center text-2xl first-letter:uppercase text-white  group-hover/item:animate-bounce">{pokemonInfo.name}</p>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,14 @@
     let filteredPokemon = null;
     let storeLoaded;
 
-    onMount(() => {
-        pokemonList.subscribe((value) => {
-            storeLoaded =  value;
-        });
+    onMount(async () => {
+        storeLoaded = await $pokemonList
     });
 
     $ : {
         // This will re-run as part of the reactivity any time something changes in this scope
         if (searchTerm){
-            filteredPokemon = $pokemonList.filter(poke => poke.name.includes(searchTerm.toLowerCase()))
+            filteredPokemon = $pokemonList.filter(poke => poke.name.includes(searchTerm.toLowerCase()) || poke.id == searchTerm)
         } else {
             filteredPokemon = [...$pokemonList]
         }
@@ -34,11 +32,16 @@
     <input bind:value={searchTerm} type="text" placeholder="Search your pokemon" class="w-full my-8 bg-slate-50 rounded-lg p-4 text-lg border border-slate-300 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300" >
 </section>
 <div class="grid grid-cols-1 gap-8 sm sm:grid-cols-2 lg:grid-cols-3">
-    {#if !storeLoaded}
-        <p transition:fade="{{ duration: 2000}}">Loading...</p>
-    {:else}
+    {#if storeLoaded}
         {#each filteredPokemon as pokemon (pokemon.id) }
             <PokemonCard pokemonInfo={pokemon} />
         {/each}
+    {:else}    
+        <div transition:fade class="w-[352px] h-52 bg-slate-500 rounded-lg animate-pulse"></div>
+        <div transition:fade class="w-[352px] h-52 bg-slate-500 rounded-lg animate-pulse"></div>
+        <div transition:fade class="w-[352px] h-52 bg-slate-500 rounded-lg animate-pulse"></div>
+        <div transition:fade class="w-[352px] h-52 bg-slate-500 rounded-lg animate-pulse"></div>
+        <div transition:fade class="w-[352px] h-52 bg-slate-500 rounded-lg animate-pulse"></div>
+        <div transition:fade class="w-[352px] h-52 bg-slate-500 rounded-lg animate-pulse"></div>
     {/if}
 </div>
