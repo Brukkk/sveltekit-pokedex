b086ee7bc8f8c3dffaa1590f3cf21623b6a36a01
Bruk Thinkpad <bruktorra@gmail.com>
2023-06-22 11:11:02 -0300

    responsive nav and footer adjust after deploy in vercel

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,7 +1,7 @@
-<nav class=" text-white flex w-full justify-around items-center gap-2 fixed bottom-0 left-0 right-0 pb-2 bg-cyan-600 z-10 shadow-lg">
+<nav class=" max-lg:text-white flex w-full justify-evenly items-center gap-2 fixed max-lg:bottom-0 lg:top-0 left-0 right-0 max-lg:pb-2 max-lg:bg-cyan-600 lg:bg-white z-50 shadow-lg">
     <a  class="text-xl" href="/">Home</a>
-    <a href="#search" class="flex items-center justify-center rounded-full p-2 z-10 -mt-4 bg-cyan-600">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-white">
+    <a href="/#search" class="flex items-center justify-center rounded-full p-2 z-10 max-lg:-mt-4 max-lg:bg-cyan-600">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 max-lg:stroke-white">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
         </svg>
     </a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,8 @@
 <div class="p-4 max-w-6xl mx-auto bg-slate-100">    
     <slot></slot>
 </div>
-<footer>
-    <p>Visit <a href="https://www.youtube.com/watch?v=UU7MgYIbtAk&ab_channel=JamesQQuick">James Q Quick</a> to learn more!</p>
+<footer class="py-4 max-lg:mb-10">
+    <p class="text-center">Visit <a class="text-blue-300" href="https://www.youtube.com/watch?v=UU7MgYIbtAk&ab_channel=JamesQQuick">James Q Quick</a> to learn more!</p>
 </footer>
 <style>
     @tailwind base;
